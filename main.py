8a095123dd916cd55301106fede53704e750c365
aditya-bobate <161609464+aditya-bobate@users.noreply.github.com>
2026-05-05 15:51:32 +0530

    Day 1: Setup task manager and basic input/output

---
+++ main.py
@@ -0,0 +1,9 @@
+#take user input
+name = input("What is your name? ")
+#using f-strings for cleaner formatting.
+print(f"Welcome {name}!")
+#show intro messages
+print("This is your task manager.")
+print("Let's start managing your tasks for the day.")
+goal = input("What is your main goal for today? ")
+print(f"let's focus on: {goal}")
