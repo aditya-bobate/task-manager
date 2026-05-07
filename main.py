8adf73d891522f27955a5cb2d64f9e5d8e365cd3
aditya-bobate <161609464+aditya-bobate@users.noreply.github.com>
2026-05-07 16:58:29 +0530

    Day 2 : How data moves through program

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 #take user input
 name = input("What is your name? ")
+age = int(input("What is your age? "))
+goal = input("What is your main goal for today? ")
 #using f-strings for cleaner formatting.
 print(f"Welcome {name}!")
+print(f"Your are {age} years old.")
+print(f"Today's main goal: {goal}")
 #show intro messages
 print("This is your task manager.")
 print("Let's start managing your tasks for the day.")
-goal = input("What is your main goal for today? ")
-print(f"let's focus on: {goal}")
